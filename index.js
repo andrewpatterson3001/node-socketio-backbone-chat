21e215f16163d787c4a612dd024e7ac6ddc881f1
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-03-09 14:59:03 -0800

    set view engine as ejs, replace index.html with views/index.ejs

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 io.on('connection', function(socket){
